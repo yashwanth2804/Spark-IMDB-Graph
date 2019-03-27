cd91b05bf57ea20b7c07e542cd31c84ae1a919ce
kambala yashwanth <kambaalayashwanth@gmail.com>
2019-03-28 01:11:03 +0530

    Added progress bar

--- DATA_EXTRACT/ImdbTitleScrapper.py
+++ DATA_EXTRACT/ImdbTitleScrapper.py
@@ -2,6 +2,8 @@ import csv
 import itertools
 from lxml import html
 import requests
+import os
+from progress.bar import Bar
 
 from ImdbCastScrapper import getCast
  
@@ -15,13 +17,17 @@ from ImdbCastScrapper import getCast
 
 pageUrl = 'https://www.imdb.com/chart/top'
 page = requests.get(pageUrl)
-tree = html.fromstring(page.content) #
-print(type(tree)) 
+tree = html.fromstring(page.content) 
 actors =[] 
 moviesCast=[]
 MoviesList = tree.xpath('//td[@class="titleColumn"]')
 #[<Element td at 0x7f4a175decb0>, <Element td at 0x7f4a175ded08>, .....]
-MoviesList = MoviesList[:1]
+
+if not os.path.exists("../DATA"):
+    os.makedirs("../DATA")
+
+
+bar = Bar('Processing', max=250)
 
 for i in MoviesList:
         title_url = i.xpath('./a/@href')
@@ -31,12 +37,16 @@ for i in MoviesList:
         movieCast = getCast(title)
         actors.append(movieCast)
         moviesCast.append(','.join(movieCast))
+        bar.next()
 
 merged = list(itertools.chain(*actors))
 uniqueActors = set(merged)
 
+
+
+
 # Saving actos uniqe list
-with open('actorsTelugu.csv','wb') as file:
+with open('../DATA/Actors.csv','wb+') as file:
     for line in uniqueActors:
         file.write(line)
         file.write('\n')
@@ -45,9 +55,11 @@ file.close()
 
 
 # Saving MovieCasts uniqe list
-with open('castsTelugu.csv','wb') as file1:
+with open('../DATA/MoviesCast.csv','wb+') as file1:
     for line in moviesCast:
         file1.write(line)
         file1.write('\n')
 
 file1.close()
+
+bar.finish()
