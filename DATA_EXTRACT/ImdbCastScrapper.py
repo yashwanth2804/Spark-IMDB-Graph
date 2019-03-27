de049fa2735c26b2d51fe968a6f73d89812df94c
kambala yashwanth <kambaalayashwanth@gmail.com>
2019-03-28 01:11:47 +0530

    updated getCast method

--- DATA_EXTRACT/ImdbCastScrapper.py
+++ DATA_EXTRACT/ImdbCastScrapper.py
@@ -9,7 +9,7 @@ def getCast(titleid):
 	tree = html.fromstring(page.content)
 	Casts = tree.xpath('//table[@class="cast_list"]/tr/td[@class="primary_photo"]')	 # [<Element td at 0x7fee28ed97e0>, <Element td at 0x7fee28ed9838>,....]
 	for i in Casts:
-		name = ''.join(i.xpath('./a/img/@alt')) # Robert Downey Jr
+		name = ''.join(i.xpath('./a/img/@alt')).encode('utf-8').strip() # Robert Downey Jr
 		casts.append(name)					
 		
 	return casts;
